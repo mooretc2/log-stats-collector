2ca0c4cabe7890ea2fdbd5f0e064e4aa6710a621
mooretc2 <mooretc2@miamioh.edu>
2019-03-14 22:54:30 -0700

    Switching to postgres, contents stored in db

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express'),
       app = express(),
-      redis = require('redis'),
-      client = redis.createClient(),
+      { Pool } = require('pg'),
+      pool = new Pool({user: 'postgres', database: 'postgres'}),
       fs = require('fs'),
       readline = require('readline');
 
-client.on('connect', () => {
-    console.log('Redis connected');
-});
-
-client.on('error', () => {
-    console.log('Error with Redis');
-});
+pool.query('CREATE TABLE IF NOT EXISTS requests(' +
+        'id serial PRIMARY KEY,' +
+        'stamp TIMESTAMP NOT NULL,' +
+        'username VARCHAR (255),' +
+        'code SMALLINT,' +
+        'path VARCHAR (255));', (err, res) => {
+            console.log(err, res);
+        })
 
 const rl = readline.createInterface({
     input: fs.createReadStream('access.log'),
     crlfDelay: Infinity
 });
 
+const MS_IN_60 = 3600000,
+      MS_IN_30 = 1800000,
+      MS_IN_15 = 900000,
+      MS_IN_5 = 300000;
+
 rl.on('line', (line) => {
-    let parts = line.split(' - ', 2)
+    let parts = line.split(' - ')
     let timestamp = parts[0]
     let year = timestamp.substring(0, 4),
         month = timestamp.substring(4, 6),
@@ -29,18 +35,32 @@ rl.on('line', (line) => {
         seconds = timestamp.substring(15, 17),
         ms = timestamp.substring(18, 21)
     let ts = new Date(year, month, date, hours, minutes, seconds, ms)
-    if(Date.now() - ts > 3600000){
-        console.log('Old: ' + ts.toString())
+    if(Date.now() - ts > MS_IN_60){
+        console.log('Old: %s', ts.toLocaleString());
         return
     }
-    console.log(ts.toString());
-    let info = parts[1]
-})
-
-client.set('test key', 'test val', redis.print);
-client.get('test key', (error, result) => {
-    if (error) {
-        console.log(error);
+    let message = parts[1],
+        user = '',
+        errorCode = null,
+        path = ''
+    
+    if(parts[1].substring(0, 5) == 'ERROR'){
+        errorCode = parts[1].substring(6, 9)
+    } else {
+        path = parts[1]
     }
-    console.log('Value: ' + result);
+    if(parts[2] !== undefined){
+        user = parts[2]
+    }
+    querystring = `INSERT INTO requests(stamp, username, code, path) VALUES
+    (to_timestamp(${ts.getTime() / 1000.0}), '${user}', ${errorCode}, '${path}');`
+    console.log(querystring)
+    console.log('Time: %s, Message: %s, User: %s', ts.toLocaleString(), message, user)
+    pool.query(querystring, (err, res) => {
+        console.log(err, res)
+    })
 });
+
+pool.query('SELECT * FROM requests;', (err, res) => {
+    console.log(err, res)
+})
